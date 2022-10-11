2f3dab5e3dd8ed9889b8f4073ee795c95c250a9e
vickyiny <108565138+vickyiny@users.noreply.github.com>
2022-10-11 13:07:55 +0530

    Go commit

---
+++ go_db-master/config/db.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-pg/pg/v9"
+
+	controllers "github.com/cavdy-play/go_db/controllers"
+)
+
+// Connecting to db
+func Connect() *pg.DB {
+	opts := &pg.Options{
+		User: "postgres",
+		Password: "password",
+		Addr: "localhost:5432",
+		Database: "krishna",
+	}
+
+	var db *pg.DB = pg.Connect(opts)
+	if db == nil {
+		log.Printf("Failed to connect")
+		os.Exit(100)
+	}
+	log.Printf("Connected to db")
+	controllers.CreateTodoTable(db)
+	controllers.InitiateDB(db)
+	return db
+}
